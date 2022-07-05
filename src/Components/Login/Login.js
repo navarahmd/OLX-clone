3de972c6bca71ff79135dfbbbef142ab6f83ecb2
Navar Ahammed <53256284+navarahmd@users.noreply.github.com>
2022-07-05 18:11:39 +0400

    Update Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,28 @@
-import React from 'react';
-
+import React,{useState,useContext} from 'react';
+import { FirebaseContext } from '../../store/FirebaseContext';
 import Logo from '../../olx-logo.png';
 import './Login.css';
+import {useHistory} from 'react-router-dom'
 
 function Login() {
+  const[email, setEmail] = useState('');
+  const[password,setPassword] = useState('');
+  const{firebase} = useContext(FirebaseContext);
+  const history = useHistory()
+  const handleLogin = (e) =>{
+    e.preventdefault()
+    firebase.auth().signInWithEmailAndPassword(email, password).then(()=>{
+      history.push('/')
+    }).catch((error)=>{
+      alert(error.message)
+    })
+  }
+
   return (
     <div>
       <div className="loginParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit={handleLogin}>
           <label htmlFor="fname">Email</label>
           <br />
           <input
@@ -17,7 +31,7 @@ function Login() {
             id="fname"
             name="email"
             defaultValue="John"
-          />
+          /> 
           <br />
           <label htmlFor="lname">Password</label>
           <br />
@@ -32,7 +46,7 @@ function Login() {
           <br />
           <button>Login</button>
         </form>
-        <a href='/signup'>Signup</a>
+        <button>Signup</button>
       </div>
     </div>
   );
